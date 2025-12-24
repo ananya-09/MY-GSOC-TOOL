a8a77a0327c60e2ecb4d051b0fb6803c7e6eb82d
Ananya <ananyar0912@gmail.com>
2025-12-24 22:57:03 +0530

    ananya-MY-GSOC-TOOL

--- libs/constants.js
+++ libs/constants.js
@@ -1,6 +1,6 @@
 
 export const IS_EDITABLE = window.location.search.includes('edit=true')
-export const OWNER = "YOUR-USERNAME";
+export const OWNER = "ananya-09";
 export const REPO = "MY-GSOC-TOOL";
-export const EMAIL = "your.email@example.com"
+export const EMAIL = "ananyar0912@gmail.com"
 export const BRANCH = "main"
